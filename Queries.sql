f08c3a40ec8ee031bdd702e44c214ab8a0480442
Fadeela Naqash <fadeela.naqash76@gmail.com>
2026-02-14 13:38:43 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -110,3 +110,120 @@ paid_amount int,
 payment_date varchar(10),
 status varchar(10)
 );
+
+insert into attendance values(1, 1, 1, '14-2-2026', 'eligible');
+insert into attendance values(2 ,2 ,2, '14-2-2026', 'NA');
+
+insert into departments values
+(1, 'MCA', 'Kaiser Sir', 222222),
+(2, 'Electronics', 'XYZ', 333333);
+
+insert into faculty values
+(1, 'Syed', 'Mohsin', 'mohsin@', 44444444, 'Professor', 1),
+(2, 'Naira', 'Shah', 'naira@', 55555555, 'Professor', 2);
+
+insert into courses values
+(1, 101, 'DBMS', 4, 1, 1, 1),
+(2, 102, 'DE', 2, 1, 1, 2);
+
+insert into enrollments values
+(1, 1, 1, 1, 2025),
+(2, 2, 2, 1, 2024);
+
+insert into exams values
+(1, 1, 'regular', '12-2-2025', 20),
+(2, 2, 'regular', '13-2-2025', 20);
+
+insert into fees values
+(1, 1, 1, 24200, 24200, '12-2-2025', 'eligible'),
+(2, 2, 1, 14400, 14400, '12-2-2025', 'eligible');
+
+insert into results values
+(1, 1, 1, 100, 'O'),
+(2, 2, 2, 100, 'O');
+
+insert into students values
+(1, 2, 'Fadeela', 'Naqash', 'fadee@', 777777, '21-09-2001', 'F', 2025, 'Admitted', 1),
+(2, 1, 'Khushboo', 'Jan', 'jan@', 888888, '1-01-2002', 'F', 2025, 'Admitted', 2);
+
+delete from attendance 
+where attendance_id = 2;
+
+delete from courses
+where course_id = 2;
+
+delete from students
+where student_id = 2; 
+
+delete from faculty
+where faculty_id = 2;
+
+delete from departments
+where department_id = 2;
+
+delete from enrollments
+where enrollment_id = 2;
+
+delete from exams
+where exam_id = 2;
+
+delete from fees
+where fee_id = 2;
+
+delete from results
+where result_id = 2;
+
+update attendance
+set date = '12-2-2024';
+
+update courses
+set semester = 2
+where course_id = 1;
+
+update students
+set roll_no = 1
+where student_id = 1;
+
+update faculty
+set phone = 888888
+where faculty_id = 1;
+
+update departments
+set office_phone = 1111111
+where department_id = 1;
+
+update enrollments
+set semester = 2
+where enrollment_id = 1;
+
+update exams
+set exam_date = '12-2-2026'
+where exam_id = 1;
+
+update fees
+set semester = 2
+where fee_id = 1;
+
+update results
+set grade = 'O+'
+where result_id = 1;
+
+drop table attendance;
+
+drop table courses;
+
+drop table students;
+
+drop table faculty;
+
+drop table fees;
+
+drop table results;
+
+drop table enrollments;
+
+drop table exams;
+
+drop table departments;
+
+drop database ums;
